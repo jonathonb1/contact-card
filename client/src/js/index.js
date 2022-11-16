f4b3102e6a98a4f76519a35d2eeef6a1fc0268fb
jonathon.biala@gmail.com <jonathon.biala@gmail.com>
2022-11-16 15:10:57 -0800

    added install button and functionality

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -34,6 +34,24 @@ newContactButton.addEventListener('click', event => {
  toggleForm()
 })
 
+// Install button
+const installBtn = document.getElementById('installBtn');
+
+window.addEventListener('beforeinstallprompt', (event) => {
+  event.preventDefault();
+  installBtn.style.visibility = 'visible';
+
+  installBtn.addEventListener('click', () => {
+    event.prompt();
+    installBtn.setAttribute('disabled', true);
+    installBtn.textContent = 'Installed!';
+  });
+});
+
+window.addEventListener('appinstalled', (event) => {
+  console.log('👍', 'appinstalled', event);
+});
+
 form.addEventListener('submit', (event) => {
   // handle the form data
   event.preventDefault();
