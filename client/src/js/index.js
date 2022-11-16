4493c191eab160cb26ef6427a149477f4cfa8598
jonathon.biala@gmail.com <jonathon.biala@gmail.com>
2022-11-16 13:26:00 -0800

    service worker added

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -100,3 +100,8 @@ window.editCard = (e) => {
   submitBtnToUpdate = true;
 };
 
+if ('serviceWorker' in navigator) {
+  // Use the window load event to keep the page load performant
+  window.addEventListener('load', () => {
+  navigator.serviceWorker.register('./service-worker.js');
+})};
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,8 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+const {InjectManifest} = require('workbox-webpack-plugin');
+
 
 module.exports = {
   mode: 'development',
@@ -9,13 +12,16 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-      title: 'Webpack Plugin',
-    }),
-
-  ],
+plugins: [
+  new HtmlWebpackPlugin({
+    template: './index.html',
+    title: 'Webpack Plugin',
+  }),
+  new InjectManifest({
+    swSrc: './src/sw.js',
+    swDest: 'service-worker.js',
+  }),
+],
   module: {
     rules: [
       {
@@ -39,3 +45,5 @@ module.exports = {
     ],
   },
 };
+
+
